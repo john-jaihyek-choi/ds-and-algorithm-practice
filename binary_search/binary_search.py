e67f7f4b71295b46f299ab41dfddddf36f0d9d23
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-01 14:44:00 -0500

    binary_search.py complete

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -2,7 +2,22 @@ from collections import defaultdict, deque
 from typing import List, Dict, DefaultDict, Set
 import time
 
-class Solution:
+class Solution2: # alternative way to calculate mid point in case of overflow
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                return mid
+
+        return -1
+    
+class Solution1:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
 
@@ -18,7 +33,7 @@ class Solution:
         return -1
     
 
-solution = Solution()
+solution = Solution1()
 start_time = time.time()
 print(solution.search([-1,0,2,4,6,8], 0))
 print("--- %s seconds ---" % (time.time() - start_time))
