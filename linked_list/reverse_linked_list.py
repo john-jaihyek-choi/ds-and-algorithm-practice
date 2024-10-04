67d4fe1cc7cc562709d24a5204158697b0a874ae
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-04 00:10:21 -0500

    reverse_linked_list.py iterative and recursive solution complete

---
+++ linked_list/reverse_linked_list.py
@@ -0,0 +1,68 @@
+import pprint
+from collections import defaultdict, deque
+from typing import List, Dict, DefaultDict, Set, Optional
+import time
+import math
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def to_list(head: ListNode):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+
+#   
+# [0, 3, 2, 1]
+# Solution 2: Recursive (TC: O(n) / SC: O(n))
+class Solution2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return new_head
+
+
+# 1
+# p  c t
+# n [3,2,1,0]
+# 2
+#   p  c  t
+# [ 3, 2, 1, 0 ]
+# 3
+#      p  c  t 
+# [ 3, 2, 1, 0 ]
+
+# Solution 1: Iterative (TC: O(n) / SC: O(1))
+class Solution1:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new, cur = None, head
+
+        while cur:
+            temp = cur.next
+            cur.next = new
+            new = cur
+            cur = temp
+        
+        return new
+            
+
+node4 = ListNode(3, None)
+node3 = ListNode(2, node4)
+node2 = ListNode(1, node3)
+node1 = ListNode(0, node2)
+solution = Solution2()
+start_time = time.time()
+answer = solution.reverseList(node1)
+pprint.pprint(to_list(answer))
+print("--- %s seconds ---" % (time.time() - start_time))
