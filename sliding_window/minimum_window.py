4322a9426b013410d1aa408aca608852ed3b37c1
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-06 18:43:20 -0500

    retried minimum_window.pyg

--- sliding_window/minimum_window.py
+++ sliding_window/minimum_window.py
@@ -3,42 +3,65 @@ from typing import List, Dict, DefaultDict, Set
 import time
 
 def minWindow(s: str, t: str) -> str:
-    if len(t) > len(s):
-        return "" 
+    # sliding window problem
+    # the window of string s will ATLEAST be length of t
+    # characters in s doesn't necessarily have to be in the order that the characters appear in t
 
-    t_count = defaultdict(int)
-    window_count = defaultdict(int)
-    
-    for c in t:
-        t_count[c] += 1
+    # Questions:
+        # are the input s and t ever going to be in different casing?
+            # ex) s = "ABC" t = "abc"
+        
+    # Assuming the answer to the question above is Yes,
+        # I'll have to add a standardization when comparing the two substrings
 
-    shortest_substring_length = float('inf')
-    shortest_substring_pair = [-1, -1]
+    # variables to store:
+        # l and r
+            # l will be the left pointer
+            # r will be the right pointer
+        # the l and r pointer index of the shortest substring
+        # count of the each letters in t
+        # count of the each letters in s that are in t
 
-    have, need = 0, len(t_count)
+    # high-level:
+        # iterate r as until all matching characters in t had been found
+        # increment l until matching characters are found
+        # in every iteration keep the record of the valid shortest substring
+        # return the substring of the shortest l,r pair
 
+    if len(s) < len(t):
+        return ""
 
-    l = 0
+    s_map = defaultdict(int)
+    t_map = defaultdict(int)
+
+    shortest_substring_index = [-1, -1]
+
+    for i in range(len(t)):
+        t_map[t[i]] += 1
+
+    window_match_count, t_count = 0, len(t_map)
+
+    l, shortest_substring = 0, float('inf')
     for r in range(len(s)):
-        window_count[s[r]] += 1
+        s_map[s[r]] += 1
 
-        if s[r] in t_count and window_count[s[r]] == t_count[s[r]]:
-            have += 1
+        if s[r] in t_map and s_map[s[r]] == t_map[s[r]]:
+            window_match_count += 1
 
-        while have >= need:
-            if r - l + 1 < shortest_substring_length:
-                shortest_substring_length = r - l + 1
-                shortest_substring_pair = [l, r]
+        while window_match_count == t_count:
+            if shortest_substring > r - l + 1:
+                shortest_substring = r - l + 1
+                shortest_substring_index = [l, r]
+            s_map[s[l]] -= 1
 
-            window_count[s[l]] -= 1
-
-            if s[l] in t_count and window_count[s[l]] < t_count[s[l]]:
-                have -= 1
+            if s[l] in t_map and s_map[s[l]] < t_map[s[l]]:
+                window_match_count -= 1
 
             l += 1
+            
 
-    l, r = shortest_substring_pair
-    return s[l:r + 1] if shortest_substring_length != float("inf") else ""
+    l, r = shortest_substring_index
+    return s[l:r + 1]
 
     # Note:
         # given string s and t
