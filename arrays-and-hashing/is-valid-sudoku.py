23eede2a6c6d76a58cf80c4e1433ce442c3b04d6
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-06 16:06:45 -0500

    retried is-valid-sudoku.py

--- arrays-and-hashing/is-valid-sudoku.py
+++ arrays-and-hashing/is-valid-sudoku.py
@@ -5,6 +5,31 @@ import time
 ROW_SIZE = 9
 COLUMN_SIZE = 9
 
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_set = defaultdict(set)
+        col_set = defaultdict(set)
+        subgrid_set = defaultdict(set)
+
+        for row in range(ROW_SIZE):
+            for col in range(COLUMN_SIZE):
+                subgrid_index = ((row // 3) * 3) + (col // 3)
+                cell = board[row][col]
+                if cell == ".":
+                    continue
+
+                if (
+                    cell in row_set[row] or
+                    cell in col_set[col] or
+                    cell in subgrid_set[subgrid_index]
+                ):
+                    return False
+
+                row_set[row].add(cell)
+                col_set[col].add(cell)
+                subgrid_set[subgrid_index].add(cell)
+        
+        return True
 
 # Solution 1
 # def isValidSudoku(board: List[List[str]]) -> bool:
@@ -111,17 +136,16 @@ def isValidSudoku(board: List[List[str]]) -> bool:
     return True
 
 
-
+solution = Solution()
 start_time = time.time()
-print(isValidSudoku([
- ["1","2",".",".","3",".",".",".","."],
- ["4",".",".","5",".",".",".",".","."],
- [".","9","8",".",".",".",".",".","3"],
- ["5",".",".",".","6",".",".",".","4"],
- [".",".",".","8",".","3",".",".","5"],
- ["7",".",".",".","2",".",".",".","6"],
- [".",".",".",".",".",".","2",".","."],
- [".",".",".","4","1","9",".",".","8"],
- [".",".",".",".","8",".",".","7","9"]
- ]))
+print(solution.isValidSudoku([
+    ["1","2",".",".","3",".",".",".","."],
+    ["4",".",".","5",".",".",".",".","."],
+    [".","9","8",".",".",".",".",".","3"],
+    ["5",".",".",".","6",".",".",".","4"],
+    [".",".",".","8",".","3",".",".","5"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".",".",".",".",".",".","2",".","."],
+    [".",".",".","4","1","9",".",".","8"],
+    [".",".",".",".","8",".",".","7","9"]]))
 print("--- %s seconds ---" % (time.time() - start_time))
