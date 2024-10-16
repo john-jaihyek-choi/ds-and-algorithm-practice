5d20812fca690738028344bb88879a85a736f676
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-16 01:50:53 -0500

    reorder_list.py solution 1 complete

---
+++ linked_list/reorder_list.py
@@ -0,0 +1,78 @@
+import time
+import math
+from collections import defaultdict, deque
+from typing import List, Dict, DefaultDict, Set, Optional
+from helper.functions import LinkedList, ListNode, Utility
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution1:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # Brainstorm:
+            # the pattern is pretty much alternating between first in the list and last in the list
+            # if I have access to the tail, I'd be able to start from each end.
+                # However, because the input is a singly linked list, I won't have access to tail nor the prev nodes
+            # if space is not a concern,
+                # I can iterate the nodes and store the value of the nodes in an array,
+                    # then from the array, create a new node in a particular order.
+
+        # 1. iterating and storing the nodes in array:
+            # if head is None:
+                # return
+            # initialize an empty array to store the nodes (node_list)
+            # initialize node at head
+            # iterate on the node until node is None
+                # in each iteration;
+                    # store each node in the array
+                    # set node to node.next
+            # initialize a 2 pointers, l and r
+                # l starting from 0
+                # r starting from len(node_list) - 1
+            # while l < r:
+                # store a temp variable for left node's next node (temp)
+                # set left node's next to right node
+                # set right node's next to temporary node stored
+                # increment l by 1
+                # decrement r by 1
+            # (IMPORTANT) disconnect the end of left list's next to None
+                # this is required to disconnect the cycle connecting left list's next from pointing to the original next value
+            # return
+
+        if not head:
+            return
+
+        node_list = []
+
+        node = head
+        while node:
+            node_list.append(node)
+            node = node.next
+
+        l, r = 0, len(node_list) - 1
+        while l < r:
+            temp = node_list[l].next
+
+            node_list[l].next = node_list[r]
+            node_list[r].next = temp
+            
+            l += 1
+            r -= 1
+        
+        node_list[l].next = None
+        
+        return 
+
+
+input1 = LinkedList([2,4,6,8])
+Utility.print_linked_list(input1.head)
+
+solution = Solution1()
+start_time = time.time()
+answer = solution.reorderList(input1.head)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+Utility.print_linked_list(answer)
