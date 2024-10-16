7663d7a0f051679370736f0dde96d605c1950b4b
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-15 22:21:54 -0500

    merge two sorted list complete

--- linked_list/merge_two_sorted_list.py
+++ linked_list/merge_two_sorted_list.py
@@ -4,15 +4,27 @@ from collections import defaultdict, deque
 from typing import List, Dict, DefaultDict, Set, Optional
 from helper.functions import LinkedList, ListNode, Utility
 
-# Solution 2: Recursive method
-class Solution1:
+# Solution 2: Recursive method (TC: O(m + n) / SC: O(m + n))
+class Solution2:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         # Pseudocode:
             # 
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+        
 
 
     
-# Solution 1: Iterative method
+# Solution 1: Iterative method (TC: O(m + n) / SC: O(1))
 class Solution1:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         # Note:
@@ -39,11 +51,11 @@ class Solution1:
 
         new_node = node = ListNode()
         
-        while list1 and list2: # O(n)
-            if list1.val <= list2.val: # O(1)
+        while list1 and list2: # O(m + n)
+            if list1.val <= list2.val: # O(m)
                 node.next = list1 # O(1)
                 list1 = list1.next # O(1)
-            else: # O(1)
+            else: # O(n)
                 node.next = list2 # O(1)
                 list2 = list2.next # O(1)
             node = node.next # O(1)
@@ -59,7 +71,7 @@ input2 = LinkedList([1, 3, 5])
 Utility.print_linked_list(input1.head)
 Utility.print_linked_list(input2.head)
 
-solution = Solution1()
+solution = Solution2()
 start_time = time.time()
 answer = solution.mergeTwoLists(input1.head, input2.head)
 print("--- %s seconds ---" % (time.time() - start_time))
