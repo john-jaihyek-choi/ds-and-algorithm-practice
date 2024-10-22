e5bc9a88d808852e14c306e4084c80afc7297e60
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-21 23:25:43 -0500

    move_zeros.py complete

--- two_pointers/move_zeros.py
+++ two_pointers/move_zeros.py
@@ -8,44 +8,80 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # In-place solution
         # input:
             # nums: List[int]
         # goal: return the updated input nums array
             # all 0's must be moved to the end of the list
+        # brainstorm:
+            # using two pointers to switch numbers
+                # when should the number switch?
+                    # when nums[r] is not 0
+                        # what if nums[l] and nums[r] are both 0?
+                            # move the r to next number. eventually l will reach the skipped 0
+                # where should each pointers begin?
+                    # l and r both 0
+                # when should the loop stop?
+                    # when r pointer reaches the end of the line
         # Two-pointer approach:
             # l and r pointer
                 # when does l move?
-                    # l moves when nums[l] is not 0
+                    # when swap happens
                 # when does r move?
-                    # r moves each iteration
-            # loop while r is less than the length of the nums
+                    # every iteration
         # Variables to track:
             # l: int
             # r: int
         # Pseudocode:
-            # initialize a l and r pointer
-                # l and r starting from 0
-            # iterate the nums array while r less than length of the nums
-                # if nums[l] is not 0:
-                    # increment l by 1
-                # if nums[l] equal to 0 and nums[r] not equal to 0
-                    # switch nums[l] with nums[r]
-                # increment r by 1
-            # return the nums array
+            # condition guard to return output if length of the input array is less than 2
+            # initialize the l pointer
+                # l = 0
+            # iterate while r is less than the length of the nums array
+                # if nums[r] != 0:
+                    # switch nums[l] and nums[r]
+                    # increment l
+            # return nums
         
-        if len(nums) <= 1:
+        if len(nums) < 2:
             return nums
-
-        l, r = 0, 0
-        while r < len(nums):
-            if nums[r] != 0 and nums[l] == 0:
-                temp = nums[l]
-                nums[l], nums[r] = nums[r], temp
-            r += 1
-
-            
-
+        
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+        
         return nums
+
+class Solution1:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+    # extra array approach:
+                # initialize an empty array 
+                # initialize a zero counter at 0
+                # iterate on the input array
+                    # append any non-zero value to the empty array
+                    # when a number is 0, increment the zero counter
+                # iterate zero counter many times:
+                    # append 0 to the empty array
+                # return empty array
+        res = []
+        zero_counter = 0
+
+        for n in nums:
+            if n != 0:
+                res.append(n)
+            else:
+                zero_counter += 1
+
+        for i in range(zero_counter):
+            res.append(0)
+
+        return res
+        
+        
         
 
 solution = Solution()
