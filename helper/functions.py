b116ca5eb96851c5ee3a8ad87e04aaf353722b49
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-29 01:21:19 -0500

    treenode class added

--- helper/functions.py
+++ helper/functions.py
@@ -15,6 +15,12 @@ class LinkedList:
             prev = new_node
 
         return prev
+    
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Utility:
     @staticmethod
