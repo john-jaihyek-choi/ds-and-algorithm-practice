716fa08d05085d34e2aec728ccd3c8587470be9f
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-28 20:48:52 -0500

    minimum eating speed retried

--- binary_search/minimum_eating_speed.py
+++ binary_search/minimum_eating_speed.py
@@ -3,6 +3,78 @@ from typing import List, Dict, DefaultDict, Set
 import time
 import math
 
+# Retried 10/28/2024: Leetcode 875
+class Solution3:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # input:
+            # piles: List[int]
+                # piles[i] = number of banas in given pile (index)
+            # h: int
+                # hours until the guard comes back to take the food away
+        # goal:
+            # return the minimum banana-per-hour eating rate needed to finish all the bananas in the pile within h hours
+        # Brainstorm:
+            # Bruteforce:
+                # Check every possibilities from 1 to n where n = max(piles)
+                # O(n^2) time solution due to checking possibility
+            # Area of optimization:
+                # max(piles) is available in O(n) time operation, so we can't get much better
+                # However, I can optimize the search part of the logic by binary search approach
+                    # max(piles) represents the maximum number of bananas koko could finish in an hour to eat at a least amount of h
+                        # when koko eats at k = max(piles), then the amount it takes for koko to fininsh will be n where n = len(piles)
+            # Optimal approach (Binary Search):
+                # iterate as much as max(piles) from 1 to max(piles)
+                    # in each iteration, use binary search to find the minimum k where k = minimum banana to finish bananas in piles array
+                        # update k to min_bananas as long as it's less than or equal to h
+                    # if m banana eating rate to finish the entire pile is > h, search left half of the max(piles) range
+                    # if m banana eating rate to finish the entire pile is < h, search the right half of the max(piles) range
+        # Variable:
+            # l: int
+                # left boundary of the max(piles) range
+            # r: int
+                # right boundary of the max(piles) range
+            # k: int
+                # number of bananas to compute the minimum integer k
+            # min_bananas: int
+                # contains the min k banana eating rate value
+        # Psuedocode:
+            # initialize min_bananas at max(piles)
+            # initialize a left and right boundary
+                # l = 1
+                # r = min_bananas
+            # while l <= r:
+                # find the mid point of the boundary:
+                    # k = (l + r) // 2
+                # keep track of the time to finish the entire pile
+                    # time_to_finish = 0
+                # iterate on piles array (bananas = piles[i]):
+                    # compute the total hours it takes to finish piles eating at k rate
+                        # time_to_finish += math.ceil(bananas / m)
+                # if time_to_finish <= h:
+                    # update min_bananas to min(min_bananas, time_to_finish)
+                    # r = k - 1
+                # if time_to_finish > h:
+                    # l = k + 1
+            # return the min_bananas
+
+        # TC: O(n * log m) / SC: O(1)
+        min_bananas = max(piles) # O(n)
+        l, r = 1, min_bananas
+        while l <= r: # O(log m) where m = maximum value in the piles array
+            k = (l + r) // 2
+            time_to_finish = 0
+            for bananas in piles: # O(n)
+                time_to_finish += math.ceil(bananas / k)
+            
+            if time_to_finish <= h:
+                min_bananas = min(min_bananas, k)
+                r = k - 1
+            else:
+                l = k + 1
+        
+        return min_bananas
+
+
 # Note:
     # 1. piles.length <= h
     # 2. k = the number of banana to eat per hr
