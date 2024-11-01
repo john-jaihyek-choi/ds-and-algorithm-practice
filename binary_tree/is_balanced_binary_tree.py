c7a15574869df11ce560741e7f0294006538ed39
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-10-31 20:41:00 -0500

    retried is_balanced_binary_tree

--- binary_tree/is_balanced_binary_tree.py
+++ binary_tree/is_balanced_binary_tree.py
@@ -11,6 +11,80 @@ import time
 #         self.left = left
 #         self.right = right
 
+# Brutforce (TC: O(n^2) / SC: O(h)):
+    # compare longest height of l and longest right of r
+        # if the difference > 1, return False
+# Pseudocode:
+    # helper check height function:
+        # base case:
+            # if not root:
+                # return 0
+        # recursion:
+            # traverse left:
+                # left = dfs(root.left)
+            # traverse right:
+                # right = dfs(root.right)
+            # return max(left, right) + 1
+    # main function:
+        # recurse on left and get max l height
+            # l_max
+        # recurse on right and get max r height
+            # r_max
+        # if abs(l_max - r_max) > 1:
+            # return False
+        # else:
+            # recurse the main funcion passing left node
+                # l_balanced = main(node.left)
+            # recurse the main funcion passing left node
+                # r_balanced = main(node.right)
+        # return l_balanced and r_balanced
+
+class Solution4:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        l_height = self.check_height(root.left)
+        r_height = self.check_height(root.right)
+
+        if abs(l_height - r_height) > 1:
+            return False
+        
+        l_balanced = self.isBalanced(root.left)
+        r_balanced = self.isBalanced(root.right)
+
+        return l_balanced and r_balanced
+        
+    def check_height(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        left = self.check_height(root.left)
+        right = self.check_height(root.right)
+
+        return max(left, right) + 1
+
+# Ideas to optimize:
+    # how do I prevent checking height O(n) operation each state?
+        # pass height and balance state from the top of the call stack
+# TC: O(n) / SC: O(1)
+class Solution3:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        _, bal = self.check_h_and_bal(root)
+
+        return bal
+    
+    def check_h_and_bal(self, root: Optional[TreeNode]) -> List[int | bool]:
+        if not root:
+            return [0, True]
+
+        l_h, l_bal = self.check_h_and_bal(root.left)
+        r_h, r_bal = self.check_h_and_bal(root.right)
+
+        return [max(l_h, r_h) + 1, abs(l_h - r_h) <= 1 and l_bal and r_bal]
+        
+
 # Optimal solution:
 # TC: O(n) / SC: O(h)
 class Solution2:
@@ -74,7 +148,7 @@ class Solution1:
         
         return max(left,right) + 1
 
-root = build([1,2,2,3,None,None,3,4,None,None,4])
+root = build([1,2,2,3,None,None,3,4,None,None, 4, None])
 print(root)
 solution = Solution2()
 start_time = time.time()
