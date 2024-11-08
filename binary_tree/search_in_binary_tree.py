64ade2cba32bc526e6285005501e1f8e8aec577c
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-11-08 15:12:03 -0600

    search in binary tree complete

---
+++ binary_tree/search_in_binary_tree.py
@@ -0,0 +1,62 @@
+from collections import defaultdict, deque
+from typing import List
+from helper.functions import TreeNode
+from typing import Optional
+from binarytree import build
+import time
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# idea:
+            # traverse DFS with the condition:
+                # if node.val == val:
+                    # return node
+                # elif node.val < val:
+                    # traverse right of the node
+                # else:
+                    # traverse the left of the node
+
+# Iterative DFS - TC O(log n) / SC: O(h)
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # iterative DFS:
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+
+            if node:
+                if node.val == val:
+                    return node
+                if node.val < val:
+                    stack.append(node.right)
+                else:
+                    stack.append(node.left)
+        
+    
+# Recursive DFS - TC O(log n) / SC: O(h)
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # recursive DFS
+        if not root:
+            return root
+
+        if root.val == val:
+            return root
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
+        
+
+root = build([1,2,2,3,None,None,3,4,None,None,4])
+print(root)
+solution = Solution()
+start_time = time.time()
+print(solution.diameterOfBinaryTree(root))
+print("--- %s seconds ---" % (time.time() - start_time))
