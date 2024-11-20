f5a34262ebd54f1f72421426b4c42b8f8a4fe267
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-11-19 18:07:47 -0600

    retried max operations

--- two_pointers/max_operations.py
+++ two_pointers/max_operations.py
@@ -3,6 +3,113 @@ from collections import defaultdict, deque
 from typing import List, Dict, DefaultDict, Set
 
 # Leetcode 1679:
+class Solution3:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        # Note:
+            # input:
+                # nums: List[int]
+                # k: int
+            # output:
+                # output: int
+            # goal:
+                # given list of int, nums, and an integer, k, return the count of sum pairs that can be removed where the sum of the two pairs == k
+        # Potential edge-cases:
+            # empty array:
+                # contrainst guarantees atleast 1 item in nums
+            # len(nums) < 2:
+                # return 0
+            # Can use the same element twice?
+                # Will assume that it cannot
+        # Idea:
+            # brute-force:
+                # 2 nested loops (i and j as indicies)
+                    # i starts from 0
+                    # j = i + 1
+            # Two-pointer approach:
+                # Assumes use of sort is allowed
+                # sort the nums array
+                # use l and r to traverse the element in nums
+                    # if nums[l] + nums[r] == k:
+                        # increment counter
+                        # increment l
+                        # decrement r
+                    # elif nums[l] + nums[r] > k:
+                        # decrement r
+                    # else: (nums[l] + nums[r] < k)
+                        # increment l
+            # Two-pointer approach:
+                # Assumes use of sort is NOT allowed
+                # num_count hash map to store the count of each unique num
+                # iterate on nums
+                    # compute the complement
+                    # if complement in num_count and num_count[i] > 0 and num_count[complement] > 0:
+                        # output += 1
+                        # num_count[i] -= 1
+                        # num_count[complement] -= 1
+
+        # Brute-force (TC: O(n^2) / SC: O(n)):
+        output = 0
+        indicies_used = set()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == k and i not in indicies_used and j not in indicies_used:
+                    output += 1
+                    indicies_used.add(i)
+                    indicies_used.add(j)
+        
+        return output
+        
+        # Pseudocode:
+            # nums.sort()
+            # output = 0
+            # l, r = 0, len(nums) - 1
+            # while l < r:
+                # sum = nums[l] + nums[r]
+                # if sum == k:
+                    # output += 1
+                    # l += 1
+                    # r -= 1
+                # elif sum > k:
+                    # r -= 1
+                # else:
+                    # l += 1
+            # return output
+        
+        # TC: O(n log n) including sort, O(n) excluding sort / SC: O(1)
+        nums.sort()
+        output = 0
+        l, r = 0, len(nums) - 1
+        while l < r:
+            k_sum = nums[l] + nums[r]
+            if k_sum == k:
+                output += 1
+                l += 1
+                r -= 1
+            elif k_sum > k:
+                r -= 1
+            else:
+                l += 1
+        return output
+
+        # TC: O(n) / SC: O(n)
+        count_map = {}
+        for n in nums:
+            count_map[n] = count_map.get(n, 0) + 1
+
+        output = 0
+        for n in count_map:
+            complement = k - n
+            
+            if complement in count_map:
+                if n == complement:
+                    output += count_map[n] // 2
+                else:
+                    output += min(count_map[n], count_map[complement])
+                    count_map[n] = 0
+                    count_map[complement] = 0
+
+        return output
+
 class Solution2:
     def maxOperations(self, nums: List[int], k: int) -> int:
         # TC: O(n) / SC: O(n)
