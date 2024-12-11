fbd5d6aa6b4f19d69f0bbcd1d658101f36b07cfd
john-jaihyek-choi <choi.john.jh@gmail.com>
2024-12-11 02:10:51 -0600

    recyclable low fat products complete

---
+++ sql/recyclable_low_fat_products.sql
@@ -0,0 +1,21 @@
+-- Write your PostgreSQL query statement below
+-- Input:
+    -- product_id (pk): int
+    -- low_fats: enum
+        -- Y == low fat
+        -- N == NOT low fat
+    -- recyclable: enum
+        -- Y == recyclable
+        -- N == NOT recyclable
+-- Output:
+    -- product_id: int
+-- Goal:
+    -- given products table, return the product ids that are both low fat and recyclable
+
+-- Idea:
+    -- filter on products where low_fats == Y and recyclable == N
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y'
+    AND recyclable = 'Y'
