8fc829a47b6e15e898bfd9acf633cd4726cb262a
john-jaihyek-choi <choi.john.jh@gmail.com>
2025-04-22 23:40:48 -0500

    two-integer-sum alternative partial solution added

--- arrays-and-hashing/two-integer-sum.py
+++ arrays-and-hashing/two-integer-sum.py
@@ -1,26 +1,43 @@
 from typing import List
 
 
-def twoSum(nums: List[int], target: int) -> List[int]:
-    # create a hash map to store the compliment number at each index of nums
-    # create a list to store the sum match
-    # calculate compliment (target - nums[i]) each iteration
-        # if compliment exists in the hash map created
-            # push the two numbers (small to large) to the sum match list
-            # return the list
+class Solution1:
+    def twoSum(nums: List[int], target: int) -> List[int]:
+        """
+        # create a hash map to store the compliment number at each index of nums
+        # create a list to store the sum match
+        # calculate compliment (target - nums[i]) each iteration
+            # if compliment exists in the hash map created
+                # push the two numbers (small to large) to the sum match list
+                # return the list
+            # return
+        """
+        compliment_map = {}
 
-    # return 
-    compliment_map = {}
+        for i, num in enumerate(nums):
+            compliment = target - num
 
-    for i, num in enumerate(nums):
-        compliment = target - num
+            if compliment in compliment_map:
+                return [compliment_map[compliment], i]
 
-        if compliment in compliment_map:
-            return [compliment_map[compliment], i]
-        
-        compliment_map[num] = i
+            compliment_map[num] = i
 
 
+class Solution2:
+    def twoSum(nums: List[int], target: int) -> List[int]:
+        # WONT WORK FOR THIS SPECIFIC QUESTION SINCE RETURN VALUE SHOULD BE INDICIES PAIR RATHER THAN THE VALUES
+        nums.sort()
+        l, r = 0, len(nums) - 1
 
-print(twoSum([3,4,5,6], 7))
-print(twoSum([4,5,6], 10))
+        while l < r:
+            if nums[l] + nums[r] > target:
+                r -= 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                return [l, r]
+
+
+solution = Solution2()
+print(solution.twoSum([3, 4, 5, 6], 7))
+print(solution.twoSum([4, 5, 6], 10))
