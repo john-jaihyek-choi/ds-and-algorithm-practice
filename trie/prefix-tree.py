cc9d082f7dbb67cc58192194a83f87ec6a17fe65
john-jaihyek-choi <choi.john.jh@gmail.com>
2025-04-30 16:57:27 -0500

    prefix tree hashtable version implemented

---
+++ trie/prefix-tree.py
@@ -0,0 +1,72 @@
+import math, time, os, random, sys, re
+from collections import defaultdict, OrderedDict
+from typing import List
+
+# Leetcode 208:
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+
+class PrefixTree:
+    """
+    1. initialize PrefixTree
+        - initializes root for the prefix tree
+    2. insert
+        - iterate on word:
+            - create new TrieNode if no children by "char" exists
+            - traverse the head to the children TrieNode
+        - update the end indicator
+    3. search
+        - iterate on word:
+            - if char doesn't exist in children of the node traversing
+                - return False
+        - if at the end of the iteration, the node.end is True:
+            - return True
+        - else
+            - return false
+    4. startswith
+        - iterate on word:
+            if char doesn't exist in children of the node traversing
+                return False
+        - return True if iteration completes without exiting
+    """
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        head = self.root
+
+        for i, char in enumerate(word):
+            if char not in head.children:
+                head.children[char] = TrieNode()
+
+            head = head.children[char]
+
+        head.end = True
+
+    def search(self, word: str) -> bool:
+        head = self.root
+
+        for i, char in enumerate(word):
+            if char not in head.children:
+                return False
+
+            head = head.children[char]
+
+        return head.end
+
+    def startsWith(self, prefix: str) -> bool:
+        head = self.root
+
+        for i, char in enumerate(prefix):
+            if char not in head.children:
+                return False
+
+            head = head.children[char]
+
+        return True
