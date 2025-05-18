5a80ff4ff48c66e7980bffad3264e1bce467f8af
john-jaihyek-choi <choi.john.jh@gmail.com>
2025-05-18 18:38:59 -0500

    number of islands reviewed

--- bfs-and-dfs/number_of_islands.py
+++ bfs-and-dfs/number_of_islands.py
@@ -7,72 +7,94 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         """
         Note:
-            - What constitutes Island?
-                - 1's surrounded by 0's (up, down, left, right)
-            - edgecase:
-                - 1 big island
-                    - big chunk of 1's that's conected with 1s that's surrounded by 0's or the edges
-            - Intuition:
-                - given:
-                    - m = len(grid)
-                    - n = len(grid[0])
-                - Iterate on every available cells, if island is found, recurse using dfs to traverse 4-way direction (left, up, right, bottom)
-                - General steps:
-                    - iterate on a grid
-                        - r, c = row, col
-                        - current_cell = grid[r][c]
-                        - if current_cell == 1:
-                            - increment island count
-                            - recurse left, up, right, bottom
-                        - else (given cell is a water)
-                    - Recursion function:
-                        - parameters = r and c
-                        - visit 4 unique directions
-                            1. left:
-                                - 0 < c - 1 < n and grid[r][c-1]:
-                                - dfs(c - 1, r)
-                            2. up:
-                                - 0 < r - 1 < m and grid[r-1][c]:
-                                - dfs(c, r - 1)
-                            3. right:
-                                - 0 < c + 1 < n and grid[r][c + 1]:
-                                - dfs(c + 1, r)
-                            4. bottom:
-                                - 0 < r + 1 < m and grid[r+1][c]:
-                                - dfs(c, r + 1)
-                            5. Else:
-                                - Return exit the function
+            - m x n grid
+                - 1 <= m, n <= 300
+                - maximum 300 by 300
+            - cell values:
+                - "1": land
+                - "0": water
+            - cells outside the boundary are water
+            - find total number of islands
+        Intuition:
+            - DFS to locate size of the island
+                - initialize islands
+                - iterate on every cells
+                    - r, c = row, col
+                    - if grid[r][c] == "1":
+                        - count island
+                        - recurse and stretch 4 directionally
+                            - right
+                            - bottom
+                            - left
+                            - up
+                - return island
+            - BFS:
+                - initialize islands
+                - define q to store adjacent island coordinates
+                - iterate on every cells
+                    - if grid[r][c] == "1"
+                        - count islands
+                        - bfs to q
+                - bfs function:
+                    - while q is non-empty:
+                        - popleft from q
+                            - (r, c) pair
+                        - Check adjacent cells
+                            - if land, add to q
         """
-
-        # TC: O(R * C) / SC: O(1), O(k) for maximum dfs call stack depth
+        # TC: O(R * C) / SC: O(1)
+        islands = 0
         rows, cols = len(grid), len(grid[0])
 
+        def bfs(r: int, c: int) -> None:
+            # initialize current coordinate
+            q = deque([(r, c)])
+
+            while q:
+                r, c = q.popleft()
+
+                # check right
+                if 0 <= c + 1 < cols and grid[r][c + 1] == "1":
+                    q.append((r, c + 1))
+                    grid[r][c + 1] = 0
+                # check bottom
+                if 0 <= r + 1 < rows and grid[r + 1][c] == "1":
+                    q.append((r + 1, c))
+                    grid[r + 1][c] = 0
+                # check left
+                if 0 <= c - 1 < cols and grid[r][c - 1] == "1":
+                    q.append((r, c - 1))
+                    grid[r][c - 1] = 0
+                # check top
+                if 0 <= r - 1 < rows and grid[r - 1][c] == "1":
+                    q.append((r - 1, c))
+                    grid[r - 1][c] = 0
+
         def dfs(r: int, c: int) -> None:
-            grid[r][c] = "0"
-            # Left
-            if 0 <= (c - 1) < cols and grid[r][c - 1] == "1":
-                dfs(r, c - 1)
-            # Up
-            if 0 <= r - 1 < rows and grid[r - 1][c] == "1":
-                dfs(r - 1, c)
-            # Right
+            # flag visited land
+            grid[r][c] = -1
+
+            # recurse right
             if 0 <= c + 1 < cols and grid[r][c + 1] == "1":
                 dfs(r, c + 1)
-            # Bottom
+            # recurse bottom
             if 0 <= r + 1 < rows and grid[r + 1][c] == "1":
                 dfs(r + 1, c)
-            return
+            # recurse left
+            if 0 <= c - 1 < cols and grid[r][c - 1] == "1":
+                dfs(r, c - 1)
+            # recurse top
+            if 0 <= r - 1 < rows and grid[r - 1][c] == "1":
+                dfs(r - 1, c)
 
-        islands = 0
-        for r in range(rows):  # O(R)
-            for c in range(cols):  # O(C)
-                cur = grid[r][c]
+        q = deque()
 
-                if cur == "1":
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
                     islands += 1
-                    dfs(
-                        r, c
-                    )  # O(k) where k == number of islands. But is guaranteed to be executed exactly once for unique 1s
+                    dfs(row, col)
+                    # bfs(row, col)
 
         return islands
 
