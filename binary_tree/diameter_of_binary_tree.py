6567e078450ad60ff0638a9df0da410d5fa3c194
john-jaihyek-choi <choi.john.jh@gmail.com>
2025-05-28 22:50:41 -0500

    diameter of binary tree complete

--- binary_tree/diameter_of_binary_tree.py
+++ binary_tree/diameter_of_binary_tree.py
@@ -11,19 +11,52 @@ import time
 #         self.left = left
 #         self.right = right
 
-class Solution:
+
+class Solution2:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        """
+        Note:
+            - DFS
+                - traverse to all left and right nodes
+                - max depth is the sum of max left depth and max right depth
+        """
+
+        # DFS
+        if root is None:
+            return 0
+
+        diameter = 0
+
+        def dfs(node: Optional[TreeNode]) -> int:
+            if node is None:
+                return 0
+
+            nonlocal diameter
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            diameter = max(left + right, diameter)
+
+            return max(left, right) + 1
+
+        dfs(root)
+
+        return diameter
+
+
+class Solution1:
     # Pseudocode:
     # General idea:
-        # recurse the tree, then compare the depth and the diameter at each recursion state
+    # recurse the tree, then compare the depth and the diameter at each recursion state
     # base case:
-        # if root is None:
-            # return 0
+    # if root is None:
+    # return 0
     # recurse:
-        # recurse left 
-        # recurse right
+    # recurse left
+    # recurse right
     # compute the longer of the two:
-        # max_diameter
-        # height
+    # max_diameter
+    # height
     # return height
     # in the outer function, return max_diameter
     def __init__(self):
@@ -34,12 +67,11 @@ class Solution:
 
         return self.max_diameter
 
-
     def dfs_height(self, root: Optional[TreeNode]) -> int:
         # Base condition:
         if not root:
             return 0
-        
+
         left = self.dfs_height(root.left)
         right = self.dfs_height(root.right)
 
@@ -48,9 +80,10 @@ class Solution:
 
         return height + 1
 
-root = build([1,2,2,3,None,None,3,4,None,None,4])
+
+root = build([1, 2, 2, 3, None, None, 3, 4, None, None, 4])
 print(root)
-solution = Solution()
+solution = Solution2()
 start_time = time.time()
 print(solution.diameterOfBinaryTree(root))
 print("--- %s seconds ---" % (time.time() - start_time))
